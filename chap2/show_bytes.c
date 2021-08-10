ecc84d22bbbff24d66e776d5ccd95064d0872b26
chuanshan <chuanshanjun@gmail.com>
2021-08-10 13:31:19 +0800

    完成练习2.7

--- chap2/show_bytes.c
+++ chap2/show_bytes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef unsigned char *byte_pointer;
 
@@ -19,7 +20,7 @@ int main()
     // p2.5
     int val = 0x87654321;
     byte_pointer valp = (byte_pointer)&val;
-    show_bytes(valp, 1); /* A. -> 小端 21; 大端 87 */ 
+    show_bytes(valp, 1); /* A. -> 小端 21; 大端 87 */
     show_bytes(valp, 2); /* B. -> 小端 21 43; 大端 87 65 */
     show_bytes(valp, 3); /* C. -> 小端 21 43 65; 大端 87 65 43 */
     show_bytes(valp, 4); /* D. -> 小端 21 43 65 87; 大端 87 65 32 21 */
@@ -29,7 +30,7 @@ int main()
     // 0x00359141
     // 0000 0000 0011 0101 1001 0001 0100 0001
     int nval = 3510593;
-    
+
     // 0x4a564504
     // 0100 1010 0101 0110 0100 0101 0000 0100
     float fnval = (float)nval;
@@ -39,6 +40,13 @@ int main()
     //   01001010010101100100010100000100
     show_int(nval);
     show_float(fnval);
+
+    char str[] = "12345";
+    show_bytes(str, sizeof(str));
+
+    // p2.7 输出 61 62 63 64 65 66
+    const char *s = "abcdef";
+    show_bytes((byte_pointer)s, strlen(s));
 }
 
 void show_bytes(byte_pointer start, size_t len)
